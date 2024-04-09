701e36f54cb9f08dabd01cdb6c89787431c8c2f8
KendylHull <hullkendyl@gmail.com>
2024-04-09 12:54:30 -0400

    Created TODO project

---
+++ todo/main.py
@@ -0,0 +1,17 @@
+enterTodoPrompt = "Enter a TODO: "
+
+todos = []
+
+todo1 = input(enterTodoPrompt)
+todos.append(todo1)
+print("Thank you!")
+todo2 = input(enterTodoPrompt)
+todos.append(todo2)
+print("Thank you!")
+todo3 = input(enterTodoPrompt)
+todos.append(todo3)
+print("Thank you!")
+
+print("Current TODOS - ", todos)
+
+
