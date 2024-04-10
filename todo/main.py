faa9a2157ffa1ec85c5eef21b52f443d8f9ee30f
KendylHull <hullkendyl@gmail.com>
2024-04-10 07:00:50 -0400

    Refactored code to include while loop

--- todo/main.py
+++ todo/main.py
@@ -2,15 +2,9 @@ enterTodoPrompt = "Enter a TODO: "
 
 todos = []
 
-todo1 = input(enterTodoPrompt)
-todos.append(todo1)
-print("Thank you!")
-todo2 = input(enterTodoPrompt)
-todos.append(todo2)
-print("Thank you!")
-todo3 = input(enterTodoPrompt)
-todos.append(todo3)
-print("Thank you!")
+while True:
+    todo = input(enterTodoPrompt)
+    todos.append(todo)
 
 print("Current TODOS - ", todos)
 
