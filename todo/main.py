b8a4556a80a38ad648540d12efa4dec643792f95
KendylHull <hullkendyl@gmail.com>
2024-04-10 07:17:50 -0400

    Created an exit condition for the while loop

--- todo/main.py
+++ todo/main.py
@@ -1,11 +1,14 @@
 enterTodoPrompt = "Enter a TODO: "
+doneCreatingTodos = "Are you finished creating TODOs? "
 
 todos = []
 
-while True:
+isDone = "No"
+
+while isDone != "Yes":
     todo = input(enterTodoPrompt)
     todos.append(todo)
-
-print("Current TODOS - ", todos)
-
+    print("Current TODOS - ", todos)
+    isDone = input(doneCreatingTodos)
+    isDone = isDone.capitalize()
 
